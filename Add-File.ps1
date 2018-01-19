cd43518186936902baf2348618f62b03d38a11d2
Zhiliang Xu <zhiliaxu@microsoft.com>
2018-01-19 21:49:31 +0800

    Update Add-File.ps1

--- Add-File.ps1
+++ Add-File.ps1
@@ -3,4 +3,4 @@ C:\agents\2.126.0\externals\git\cmd\git.exe config user.email "xu.zl@live.com"
 C:\agents\2.126.0\externals\git\cmd\git.exe config user.name "Zhiliang Xu"
 C:\agents\2.126.0\externals\git\cmd\git.exe add newfile.txt
 C:\agents\2.126.0\externals\git\cmd\git.exe commit -m "add new file"
-C:\agents\2.126.0\externals\git\cmd\git.exe push
+C:\agents\2.126.0\externals\git\cmd\git.exe push origin HEAD:master
