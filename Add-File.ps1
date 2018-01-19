1302a94fc80993b9100746556732dcad5ac7cb24
Zhiliang Xu <zhiliaxu@microsoft.com>
2018-01-19 21:57:24 +0800

    Update Add-File.ps1

--- Add-File.ps1
+++ Add-File.ps1
@@ -3,4 +3,6 @@ C:\agents\2.126.0\externals\git\cmd\git.exe config user.email "xu.zl@live.com"
 C:\agents\2.126.0\externals\git\cmd\git.exe config user.name "Zhiliang Xu"
 C:\agents\2.126.0\externals\git\cmd\git.exe add newfile.txt
 C:\agents\2.126.0\externals\git\cmd\git.exe commit -m "add new file"
+git remote set-url origin https://github.com/zhiliangxu/hooktest.git
+git remote set-url --push origin https://github.com/zhiliangxu/hooktest.git
 C:\agents\2.126.0\externals\git\cmd\git.exe push origin master
