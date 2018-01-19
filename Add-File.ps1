c5dfb4c6b81dae7e901f4338eb4f1835803d5f8c
Zhiliang Xu <zhiliaxu@microsoft.com>
2018-01-19 21:45:38 +0800

    Update Add-File.ps1

--- Add-File.ps1
+++ Add-File.ps1
@@ -1,4 +1,4 @@
-Out-File -FilePath "newfile.txt" -InputObject $((New-Date).ToString()) -Encoding UTF8
+Out-File -FilePath "newfile.txt" -InputObject $((Get-Date).ToString()) -Encoding UTF8
 C:\agents\2.126.0\externals\git\cmd\git.exe add newfile.txt
 C:\agents\2.126.0\externals\git\cmd\git.exe commit -m "add new file"
 C:\agents\2.126.0\externals\git\cmd\git.exe push
