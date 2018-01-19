b2de3bcf8b67a598411ecab4cfe02f0174a01ade
Zhiliang Xu <zhiliaxu@microsoft.com>
2018-01-19 21:47:34 +0800

    Update Add-File.ps1

--- Add-File.ps1
+++ Add-File.ps1
@@ -1,4 +1,6 @@
 Out-File -FilePath "newfile.txt" -InputObject $((Get-Date).ToString()) -Encoding UTF8
+C:\agents\2.126.0\externals\git\cmd\git.exe config user.email "xu.zl@live.com"
+C:\agents\2.126.0\externals\git\cmd\git.exe config user.name "Zhiliang Xu"
 C:\agents\2.126.0\externals\git\cmd\git.exe add newfile.txt
 C:\agents\2.126.0\externals\git\cmd\git.exe commit -m "add new file"
 C:\agents\2.126.0\externals\git\cmd\git.exe push
