4b428916844370be57ad04b656d46835c590b863
Zhiliang Xu <zhiliaxu@microsoft.com>
2018-01-20 12:45:01 +0800

    Update Add-File.ps1

--- Add-File.ps1
+++ Add-File.ps1
@@ -1,2 +1,2 @@
 Out-File -FilePath "newfile.txt" -InputObject $((Get-Date).ToString()) -Encoding UTF8
-Add-Content "$env:USERPROFILE\.git-credentials" "https://a2d842939ef0cbc657499be3f3b248a298e83031:x-oauth-basic@github.com`n"
+Add-Content "$env:USERPROFILE\.git-credentials" "https://$($env:token):x-oauth-basic@github.com`n"
